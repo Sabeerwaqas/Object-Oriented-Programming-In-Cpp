8e4e6ac4f00f781a44ac7b89f10a328db2f68e33
Sabeerwaqas <sabeerwaqas050@gmail.com>
2024-01-04 22:47:37 +0500

    [Practice classes]

--- Constructors & Class Methods/constructorsAndClass.cpp
+++ Constructors & Class Methods/constructorsAndClass.cpp
@@ -1,17 +1,52 @@
 #include <iostream>
+#include <list>
 
 using namespace std;
 
+    
+	class personData {
+		
+		// We need to put public access modifier to use these member commonly.
+		
+		public:
+		
+		// Members of class
+		
+		string name;
+		int age;
+		string profession;
+		list <string> idealFood;	
+		
+		
+	};
+
+
 int main(){
 
-    
+	/*
+	
+	What is OOP?
+	
+	OOP is a programming paraadigm that allow us to represent real life objects
+	in programming.
+	
+	*/	
+
+	personData firstCandidate;
+	
+	firstCandidate.name = "Mr. XYZ";
+	firstCandidate.age = 25;
+	firstCandidate.profession = "Lecturer";
+//	firstCandidate.idealFoods = {"Biryani", "Chicken Karahi"};
 
 
-
-
-
-
-
+	cout << "Name: " << firstCandidate.name << endl;
+	cout << "Age: " << firstCandidate.age << endl;
+	cout << "Profession: " << firstCandidate.profession << endl;
+//	cout << "Ideal Foods: " << firstCandidate.idealFood << endl;
+//	for(string ideal_food: firstCandidate.idealFood	){
+//		cout << ideal_food << endl;
+//	}
 
 
 
